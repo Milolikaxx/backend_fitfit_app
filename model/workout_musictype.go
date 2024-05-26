7fde99b81b8674cebcb6e3f38a253156a417fc4d
Milolikaxx <nusitday106773@gmail.com>
2024-05-26 12:38:45 +0700

    wpMusicType ิัwpid

--- model/workout_musictype.go
+++ model/workout_musictype.go
@@ -1,9 +1,10 @@
 package model
 
 type WorkoutMusictype struct {
-	ID   int `gorm:"column:id;primary_key;AUTO_INCREMENT"`
-	Wpid int `gorm:"column:wpid;NOT NULL"`
-	Mtid int `gorm:"column:mtid;NOT NULL"`
+	ID        int       `gorm:"column:id;primary_key;AUTO_INCREMENT"`
+	Wpid      int       `gorm:"column:wpid;NOT NULL"`
+	Mtid      int       `gorm:"column:mtid;NOT NULL"`
+	MusicType MusicType `gorm:"foreignKey:Mtid"`
 }
 
 func (m *WorkoutMusictype) TableName() string {
